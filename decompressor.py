Normalization fixed

decompressor.py
@@ -1,6 +1,4 @@
 import sys
-import os.path
-import os, psutil
 sys.path.append('./misc')
 
 from torch.utils.tensorboard import SummaryWriter
@@ -9,7 +7,6 @@ import torch
 import NNModels
 import CustomFunctions
 import numpy as np
-import torch_optimizer as optim
 
 # runtime start
 start_time = time.time()
@@ -18,21 +15,21 @@ start_time = time.time()
 print(torch.cuda.get_device_name(0)) 
 device = torch.device("cuda")
 
-# define nn params
-compressor   = NNModels.Compressor(n_feature=2002, n_hidden=512, n_hidden2=512, n_hidden3=512, n_output=32).to(device)
-decompressor = NNModels.Decompressor(n_feature=24+32, n_hidden=512, n_output=1001).to(device)
-
 # load data
 xData = CustomFunctions.LoadData("XData", True)
 yData = CustomFunctions.LoadData("YData", True)
 
 hierarchy = CustomFunctions.LoadData("HierarchyData")
-hierarchy = [int(i) for i in hierarchy[0]]
+hierarchy = [int(hierarchy[i][0]) for i in range(len(hierarchy))]
 
 # list of tensors
 xTensor = []
 yTensor = []
 qTensor = []
+pTensor = []
+yFinal = []
+qFinal = []
+pNorm  = []
 
 for i in range(len(xData)):
     xTensor.append(torch.tensor(xData[i], dtype=torch.float).to(device))
@@ -40,23 +37,47 @@ for i in range(len(xData)):
 
 # compute forward kinematics
 qTensor = CustomFunctions.ForwardKinematics(yTensor, hierarchy)
-# print()
+
+for i in range(len(yTensor)):
+    yInfo = torch.empty(yTensor[i].size(0), 0).to(device)
+    qInfo = torch.empty(qTensor[i].size(0), 0).to(device)
+
+    for j in range(0, yTensor[i].size(1), 13):
+        yInfo = torch.cat((yInfo, yTensor[i][:,j:j+3], CustomFunctions.quaternion_rotation_matrix(yTensor[i][:,j+3:j+7]), yTensor[i][:,j+7:j+13]), dim=1)
+        qInfo = torch.cat((qInfo, qTensor[i][:,j:j+3], CustomFunctions.quaternion_rotation_matrix(qTensor[i][:,j+3:j+7]), qTensor[i][:,j+7:j+13]), dim=1)
+
+    yFinal.append(yInfo)
+    qFinal.append(qInfo)
+
+for i in range(len(yData)):
+    pTensor.append(torch.cat((yTensor[i], qTensor[i]), dim=1))
+
+xTensor = CustomFunctions.StandardizeData(xTensor, dim=0)
+pNorm   = CustomFunctions.StandardizeData(pTensor, dim=0)
 
 # relevant dataset indexes
-transforms = list(range(0, 13))
-velocities = []
+lTransforms = []
+lVelocities = []
+sTransforms = []
+sVelocities = []
 
-[transforms.extend(list(range(i, i + 7))) for i in range(13, yTensor[0].size(1), 13)]
-[velocities.extend(list(range(i + 7, i + 13))) for i in range(0, yTensor[0].size(1), 13)]
+[lTransforms.extend(list(range(i + 00, i + 7))) for i in range(0, yTensor[0].size(1), 13)]
+[lVelocities.extend(list(range(i + 7, i + 13))) for i in range(0, yTensor[0].size(1), 13)]
+[sTransforms.extend(list(range(i + 00, i + 7))) for i in range(yTensor[0].size(1), pTensor[0].size(1), 13)]
+[sVelocities.extend(list(range(i + 7, i + 13))) for i in range(yTensor[0].size(1), pTensor[0].size(1), 13)]
+
+# define nn params
+compressor   = NNModels.Compressor(n_feature=yTensor[0].size(1) * 2, n_hidden=512, n_hidden2=512, n_hidden3=512, n_output=32).to(device)
+decompressor = NNModels.Decompressor(n_feature=24+32, n_hidden=512, n_output=yTensor[0].size(1)).to(device)
 
 # dataloader settings
-train = NNModels.CustomDataset((xTensor, yTensor, qTensor), window=2)
+train = NNModels.CustomDataset((xTensor, yTensor, qTensor, pNorm), window=2)
 train_loader = NNModels.Data.DataLoader(train, shuffle=True, batch_size=32)
 c_optimizer, c_scheduler = NNModels.TrainSettings(compressor)
 d_optimizer, d_scheduler = NNModels.TrainSettings(decompressor)
 
 # training settings
-epochs = 15000
+epochs = 1000
 logFreq = 500
 
 # init tensorboard
@@ -65,43 +86,55 @@ writer = SummaryWriter()
 # train the network. range = epochs
 for t in range(epochs + 1):
     epochLoss = 0.0
+    lreg = 0.0
+    sreg = 0.0
+    vreg = 0.0
+    lloc = 0.0
+    lvel = 0.0
+    lchr = 0.0
+    cvel = 0.0
+
     epoch_time = time.time()
 
-    for batch_idx, (x, y, q) in enumerate(train_loader):
+    for batch_idx, (x, y, q, p) in enumerate(train_loader):
 
-        x.to(device), y.to(device)
-        
+        x.to(device), y.to(device), q.to(device), p.to(device)
+
         # (seq. length, batch size, features) <- (batch size, seq. length, features)
         x = torch.transpose(x, 0, 1)
         y = torch.transpose(y, 0, 1)
         q = torch.transpose(q, 0, 1)
+        p = torch.transpose(p, 0, 1)
+
+        # y += np.random.normal(y.mean(dim=2), y.std(dim=2), y.shape(0), y.shape(1))
 
         # generate latent variables Z
-        p = CustomFunctions.NormalizeData(torch.cat((y, q), dim=2), dim=2)
+        # p = CustomFunctions.NormalizeData(torch.cat((y, q), dim=2), dim=1)
         zPred = compressor(p)
         
         # reconstruct pose Y
-        fData = CustomFunctions.NormalizeData(torch.cat((x, zPred), dim=2), dim=2)
-        yPred = decompressor(fData)
+        f = torch.cat((x, zPred), dim=2)
+        yPred = decompressor(f)
 
         # recompute forward kinematics
         # qPred = torch.stack(CustomFunctions.ForwardKinematics(yPred, hierarchy), dim=0)
 
         #  compute latent regularization losses
-        loss_lreg = torch.nn.MSELoss()(zPred, torch.zeros_like(zPred))
-        # loss_sreg = torch.nn.L1Loss() (zPred, torch.zeros_like(zPred))
-        # loss_vreg = torch.nn.MSELoss()((zPred[1] - zPred[0]) / 0.017, torch.zeros_like(zPred[0]))
+        loss_lreg = 0.02000 * torch.nn.MSELoss()(zPred, torch.zeros_like(zPred))
+        loss_sreg = 0.00100 * torch.nn.L1Loss() (zPred, torch.zeros_like(zPred))
+        loss_vreg = 0.00500 * torch.nn.L1Loss() ((zPred[1] - zPred[0]) / 0.017, torch.zeros_like(zPred[0]))
 
         # local & character space losses
-        loss_loc = torch.nn.L1Loss()(yPred[:,:,transforms], y[:,:,transforms])
-        # loss_chr = torch.nn.L1Loss()(qPred[:,:,transforms], q[:,:,transforms])
+        loss_loc  = 30.0000 * torch.nn.L1Loss()(yPred[:,:,lTransforms], y[:,:,lTransforms])
+        loss_lvel = 0.00100 * torch.nn.L1Loss()(yPred[:,:,lVelocities], y[:,:,lVelocities])
 
         # local & character space velocity losses
-        loss_lvel = torch.nn.L1Loss()((yPred[1,:,transforms] - yPred[0,:,transforms]) / 0.017, (y[1,:,transforms] - y[0,:,transforms]) / 0.017)
-        # loss_cvel = torch.nn.L1Loss()((qPred[1,:,transforms] - qPred[0,:,transforms]) / 0.017, (q[1,:,transforms] - q[0,:,transforms]) / 0.017)
+        # loss_chr  = 30.0000 * torch.nn.L1Loss()(qPred[:,:,lTransforms], q[:,:,lTransforms])
+        # loss_cvel = 0.00100 * torch.nn.L1Loss()(qPred[:,:,lVelocities], q[:,:,lVelocities])
 
-        # loss = loss_lreg + loss_sreg + loss_vreg + loss_loc + loss_chr + loss_lvel + loss_cvel
-        loss = loss_lreg + loss_loc + loss_lvel
+        loss = loss_lreg + loss_sreg + loss_vreg + loss_loc + loss_lvel
+        # loss = loss_lreg + loss_loc
+        # loss = loss_loc
 
         # clear gradients for next train
         c_optimizer.zero_grad()
@@ -119,37 +152,44 @@ for t in range(epochs + 1):
         d_scheduler.step()
 
         # log loss value
-        epochLoss += loss.item()
-        
+        epochLoss += loss_loc.item()
+        lreg += loss_lreg.item()
+        sreg += loss_sreg.item()
+        vreg += loss_vreg.item()
+        lloc += loss_loc.item()
+        lvel += loss_lvel.item()
+        lchr += loss_loc.item()
+        cvel += loss_lvel.item()
+
     # just printing where training is
     if t % logFreq == 0:
         print("Epoch:", t, "\tLoss:", epochLoss, "\tRuntime:", (time.time() - epoch_time) * logFreq / 60, "minutes")
-    
+        print(lreg, sreg, vreg, lloc, lvel, lchr, cvel)
     # log loss in tensorboard
     writer.add_scalar('Decompressor Loss', epochLoss, t)
 
 # save character space transforms (Q)
 with open('database/QData.txt', "w+") as f:
-    for i in range(len(qTensor)):        
-        for j in range(qTensor[i].size(0)):
-            np.savetxt(f, qTensor[i][j].cpu().detach().numpy()[None], delimiter=" ")
+    for i in range(len(pTensor)):
+        for j in range(pTensor[i].size(0)):
+            np.savetxt(f, pTensor[i][j].cpu().detach().numpy()[None], delimiter=" ")
 
         np.savetxt(f, [''], fmt='%s')
 
 # save latent variables (Z)
 with open('database/ZData.txt', "w+") as f:
     for i in range(len(yTensor)):
-        p = CustomFunctions.NormalizeData(torch.cat((yTensor[i], qTensor[i]), dim=1), dim=1)
-
-        for j in range(p.size(0)):
-            zPred = compressor(p[i])
+        # p = CustomFunctions.NormalizeData(torch.cat((yTensor[i], qTensor[i]), dim=1), dim=1)
+        
+        for j in range(pNorm[i].size(0)):
+            zPred = compressor(pNorm[i][j])
             np.savetxt(f, zPred.cpu().detach().numpy()[None], delimiter=" ")
 
         np.savetxt(f, [''], fmt='%s')
 
 # export compressor model
 torch.onnx.export(
-    compressor, torch.rand(1, 1, 2002, device=device), 
+    compressor, torch.rand(1, 1, yTensor[0].size(1) * 2, device=device), 
     "onnx/compressor.onnx", export_params=True,
     opset_version=9, do_constant_folding=True,
     input_names = ['x'], output_names = ['z']

misc/CustomFunctions.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 device = torch.device("cpu")
 
@@ -28,18 +29,86 @@ def LoadData(filename, isStepper=False):
     return data
 
 def NormalizeData(data, dim=1):
-    means = data.mean(dim=dim, keepdim=True)
-    stds  = data.std(dim=dim, keepdim=True)
-    normalized = (data - means) / stds
+    xMin = torch.empty(0, data[0].size(1), device='cuda')
+    xMax = torch.empty(0, data[0].size(1), device='cuda')
+    normalized = []
+
+    for i in range(len(data)):
+        xMin = torch.cat((xMin, torch.min(data[i], 0).values.unsqueeze(0)), dim=0)
+        xMax = torch.cat((xMax, torch.max(data[i], 0).values.unsqueeze(0)), dim=0)
+    
+    xMin = torch.min(xMin, 0).values
+    xMax = torch.max(xMax, 0).values
+    # print(xMax)
+
+    # normalization
+    for i in range(len(data)):
+        normalized.append((data[i] - xMin) / (xMax - xMin))
 
     return normalized
 
-def normalize(v, tolerance=0.00001):
-    mag2 = sum(n * n for n in v)
-    if abs(mag2 - 1.0) > tolerance:
-        mag = (mag2)**(0.5)
-        v = tuple(n / mag for n in v)
-    return v
+def StandardizeData(data, dim=1):
+    mean = 0
+    std = 0
+    count = 0
+    normalized = []
+
+    # mean
+    for i in range(len(data)):
+        mean = mean + data[i].sum(dim=dim)
+        count = count + data[i].size(0)
+
+    mean = mean / count
+    
+    # std
+    for i in range(len(data)):
+        std = std + ((data[i] - mean) ** 2).sum(dim=dim)
+    
+    std = (std / count) ** 0.5
+
+    # standardization
+    for i in range(len(data)):
+        normalized.append((data[i] - mean) / (std + 0.0001))
+
+    return normalized
+
+def quaternion_rotation_matrix(Q):
+    """
+    Covert a quaternion into a full three-dimensional rotation matrix.
+ 
+    Input
+    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
+ 
+    Output
+    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
+             This rotation matrix converts a point in the local reference 
+             frame to a point in the global reference frame.
+    """
+    # Extract the values from Q
+    q0 = Q[:,0]
+    q1 = Q[:,1]
+    q2 = Q[:,2]
+    q3 = Q[:,3]
+     
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+     
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+     
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+
+    # 3x3 rotation matrix
+    rot_matrix = torch.stack((r00, r01, r02, r10, r11, r12, r20, r21, r22), 1)
+
+    return rot_matrix
 
 def q_mult(a, b):
     aw, ax, ay, az = torch.unbind(a, -1)

misc/NNModels.py
@@ -42,30 +42,18 @@ class Decompressor(torch.nn.Module):
 # neural network model
 class Stepper(torch.nn.Module):
     # nn layers shape
-    def __init__(self, n_feature, n_hidden, n_output):
+    def __init__(self, n_feature, n_hidden, n_hidden2, n_output):
         super(Stepper, self).__init__()
-        self.n_hidden = n_hidden
-
-        self.hidden  = torch.nn.LSTM(n_feature, n_hidden, 1, batch_first=False)
-        self.hidden2 = torch.nn.LSTM(n_hidden , n_hidden, 1, batch_first=False)
-        self.predict = torch.nn.Linear(n_hidden, n_output)
+        self.hidden   = torch.nn.Linear(n_feature, n_hidden)
+        self.hidden2  = torch.nn.Linear(n_hidden, n_hidden2)
+        self.predict  = torch.nn.Linear(n_hidden2, n_output)
 
     # feed forward
-    def forward(self, x): 
-        h_t = torch.zeros(1, x.size(1), 512, device=device).requires_grad_()
-        c_t = torch.zeros(1, x.size(1), 512, device=device).requires_grad_() 
-
-        output, (h_t, c_t) = self.hidden(x, (h_t, c_t))
-        output = F.relu(output)
-
-        h_t2 = torch.zeros(1, x.size(1), 512, device=device).requires_grad_()
-        c_t2 = torch.zeros(1, x.size(1), 512, device=device).requires_grad_() 
-
-        output, (h_t2, c_t2) = self.hidden2(output, (h_t2, c_t2))
-        output = F.relu(output)
-
-        output = self.predict(output)
-        return output
+    def forward(self, x):
+        x = F.relu(self.hidden(x)) 
+        x = F.relu(self.hidden2(x))
+        x = self.predict(x)
+        return x
 
 # neural network model
 class Projector(torch.nn.Module):

projector.py
@@ -21,27 +21,33 @@ device = torch.device("cuda")
 model = NNModels.Projector(n_feature=24, n_hidden=512, n_hidden2=512, n_hidden3=512, n_hidden4=512, n_output=24+32).to(device)
 
 # load data
-xData = CustomFunctions.LoadData("XData")
-zData = CustomFunctions.LoadData("ZData")
+xData = CustomFunctions.LoadData("XData", False)
+zData = CustomFunctions.LoadData("ZData", False)
+
+# list of tensors
+xTensor = []
+zTensor = []
+combinedFeatures = []
+
+xTensor = CustomFunctions.StandardizeData([torch.tensor(xData, dtype=torch.float).to(device)], dim=0)
+zTensor.append(torch.tensor(zData, dtype=torch.float).to(device))
 
 # combined features (x + z)
-combinedFeatures = np.append(xData, zData, axis=1)
+combinedFeatures.append(torch.cat((xTensor[0], zTensor[0]), dim=1))
 
-# convert list to tensor
-x = torch.tensor(xData, dtype=torch.float).to(device)
-y = torch.tensor(combinedFeatures, dtype=torch.float).to(device)
-
-# normalize data
-normX = CustomFunctions.NormalizeData(x)
-normY = CustomFunctions.NormalizeData(y)
+# relevant dataset indexes
+xVal = [list(range(0, 9)) , list(range(12, 15)), list(range(18, 21))]
+xTrj = [list(range(0, 9))]
+xVal = sum(xVal, [])
+xTrj = sum(xTrj, [])
 
 # dataloader settings for training
-train = NNModels.Data.TensorDataset(normX, normY)
+train = NNModels.Data.TensorDataset(xTensor[0], combinedFeatures[0])
 train_loader = NNModels.Data.DataLoader(train, shuffle=True, batch_size=32)
 optimizer, scheduler = NNModels.TrainSettings(model)
 
 # training settings
-epochs = 5000
+epochs = 1000
 logFreq = 500
 
 # init tensorboard
@@ -50,6 +56,9 @@ writer = SummaryWriter()
 # train the network. range = epochs
 for t in range(epochs + 1):
     epochLoss = 0.0
+    xval = 0.0
+    zval = 0.0
+    dist = 0.0
     epoch_time = time.time()
 
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -57,32 +66,34 @@ for t in range(epochs + 1):
         data.to(device), target.to(device)
 
         # generate gaussian noise
-        # dataNoise = data + torch.randn_like(data) * torch.tensor(np.random.uniform(-0.1, 0.1, (data.size(0), data.size(1))), dtype=torch.float, device=device)
         dataNoise = torch.zeros_like(data)
         for i in range(data.size(0)):
-            dataNoise[i] = data[i] + torch.randn_like(data[i]) * np.random.uniform(0, 1)
-
-        dataNoise = CustomFunctions.NormalizeData(dataNoise)
+            dataNoise[i] = data[i] + (np.random.uniform(0.0, 1.0) * torch.randn_like(data[i]))
 
         # perform knn
         knnIndices = []
-        newTargets = torch.empty(data.size(0), 0).to(device)
 
         for i in range(data.size(0)):
-            dist = torch.norm(normX - dataNoise[i], dim=1)
-            knn = dist.topk(1, largest=False)
+            diff = torch.norm(xTensor[0] - dataNoise[i], dim=1, p=None)
+            knn = diff.topk(1, largest=False)
             knnIndices.append(knn.indices.tolist()[0])
+        
+        xk          = torch.empty(data.size(0), 0).to(device)
+        newTargets  = torch.empty(data.size(0), 0).to(device)
 
-        newTargets = torch.cat((newTargets, normY[knnIndices]), dim=1)
+        xk          = torch.cat((xk, xTensor[0][knnIndices]), dim=1)
+        newTargets  = torch.cat((newTargets, combinedFeatures[0][knnIndices]), dim=1)
 
         # feed forward
         prediction = model(dataNoise)
 
         # losses
-        loss_val  = torch.nn.L1Loss()(prediction, newTargets)   
-        loss_dist = torch.nn.L1Loss()(torch.nn.MSELoss()(prediction[:,:24], dataNoise), torch.nn.MSELoss()(newTargets[:,:24], dataNoise))   
+        loss_xval = 1.10 * torch.nn.L1Loss()(prediction[:,:24], newTargets[:,:24])  
+        loss_zval = 1.00 * torch.nn.L1Loss()(prediction[:,24:], newTargets[:,24:])  
+        # loss_dist = 0.02 * torch.nn.L1Loss()(torch.nn.MSELoss()(prediction[:,:24], dataNoise), torch.nn.MSELoss()(xk, dataNoise))  
+        loss_dist = 0.02 * torch.nn.MSELoss()(xk, dataNoise)  
 
-        loss = loss_val
+        loss = loss_xval + loss_zval
 
         optimizer.zero_grad()   # clear gradients for next train
         loss.backward()         # backpropagation, compute gradients
@@ -91,11 +102,14 @@ for t in range(epochs + 1):
 
         # log loss value
         epochLoss += loss.item()
+        xval += loss_xval.item()
+        zval += loss_zval.item()
+        dist += loss_dist.item()
     
     # just printing where training is
     if t % logFreq == 0:
         print("Epoch:", t, "\tLoss:", epochLoss, "\tRuntime:", (time.time() - epoch_time) * logFreq / 60, "minutes")
-    
+        print(xval, zval, dist)
     # log loss in tensorboard  
     writer.add_scalar('Projector Loss', epochLoss, t)
 

stepper.py
@@ -15,32 +15,50 @@ print(torch.cuda.get_device_name(0))
 device = torch.device("cuda")
 
 # define nn params
-model = NNModels.Stepper(n_feature=24+32, n_hidden=512, n_output=24+32).to(device)
+model = NNModels.Stepper(n_feature=24+32, n_hidden=512, n_hidden2=512, n_output=24+32).to(device)
 
 # load data
 xData = CustomFunctions.LoadData("XData", True)
 zData = CustomFunctions.LoadData("ZData", True)
 
 # list of tensors
-combinedFeaturesI = []
-combinedFeaturesN = []
+xTensor = []
+zTensor = []
 
 for i in range(len(xData)):
-    combinedFeaturesI.append(torch.cat((torch.tensor(xData[i]).to(device), torch.tensor(zData[i]).to(device)), dim=1))
-    combinedFeaturesN.append(torch.roll(torch.cat((torch.tensor(xData[i]).to(device), torch.tensor(zData[i]).to(device)), dim=1), -1, 0))
+    xTensor.append(torch.tensor(xData[i], dtype=torch.float).to(device))
+    zTensor.append(torch.tensor(zData[i], dtype=torch.float).to(device))
 
-    combinedFeaturesI[i] = CustomFunctions.NormalizeData(combinedFeaturesI[i], dim=1)
-    combinedFeaturesN[i] = CustomFunctions.NormalizeData(combinedFeaturesN[i], dim=1)
+xTensor = CustomFunctions.StandardizeData(xTensor, dim=0)
+
+# list of tensors
+combinedFeaturesI = []
+combinedFeaturesN = []
+combinedFeaturesD = []
+
+for i in range(len(xData)):
+    combinedFeaturesI.append(torch.cat((xTensor[i], zTensor[i]), dim=1))
+    combinedFeaturesN.append(torch.roll(torch.cat((xTensor[i], zTensor[i]), dim=1), -1, 0))
+
+    # combinedFeaturesI[i] = CustomFunctions.NormalizeData(combinedFeaturesI[i], dim=0)
+    # combinedFeaturesN[i] = CustomFunctions.NormalizeData(combinedFeaturesN[i], dim=0)
+    combinedFeaturesD.append(combinedFeaturesN[i] - combinedFeaturesI[i])
 
 print()
 
+# relevant dataset indexes
+xVal = [list(range(0, 9)) , list(range(12, 15)), list(range(18, 21))]
+xVel = [list(range(9, 12)), list(range(15, 18)), list(range(21, 24))]
+xVal = sum(xVal, [])
+xVel = sum(xVel, [])
+
 # dataloader settings for training
-train = NNModels.CustomDataset((combinedFeaturesI, combinedFeaturesN), window=20)
+train = NNModels.CustomDataset((combinedFeaturesI, combinedFeaturesD, combinedFeaturesN), window=20)
 train_loader = NNModels.Data.DataLoader(train, shuffle=True, batch_size=32)
 optimizer, scheduler = NNModels.TrainSettings(model)
 
 # training settings
-epochs = 5000
+epochs = 1000
 logFreq = 500
 
 # init tensorboard
@@ -49,28 +67,38 @@ writer = SummaryWriter()
 # train the network. range = epochs
 for t in range(epochs + 1):
     epochLoss = 0.0
+    xval = 0.0
+    zval = 0.0
+    xvel = 0.0
+    zvel = 0.0
     epoch_time = time.time()
 
-    for batch_idx, (data, target) in enumerate(train_loader):    
+    for batch_idx, (data, delta, target) in enumerate(train_loader):    
         
         data.to(device), target.to(device)
 
         # (seq. length, batch size, features) <- (batch size, seq. length, features)
         data    = torch.transpose(data, 0, 1)
         target  = torch.transpose(target, 0, 1)
-        n_batch = data.size(1)
 
-        # h_t = torch.zeros(1, n_batch, 512, device=device).requires_grad_()
-        # c_t = torch.zeros(1, n_batch, 512, device=device).requires_grad_()
+        pred = torch.empty(0, data.size(1), 56).to(device)
+        pred = torch.cat((pred, data[1].view(1, data.size(1), 56)), dim=0)
 
-        # predict x~ and z~ over a window of s frames
-        prediction= model(data)
-
+        # predict delta x and delta z over a window of s frames
+        for i in range(1, 20):
+            prediction = model(pred[i - 1])
+            pred = torch.cat((pred, (pred[i - 1] + prediction).view(1, data.size(1), 56)), dim=0)
+        
         # losses
-        loss_val = torch.nn.L1Loss()(prediction, target) 
-        loss_vel = torch.nn.L1Loss()((prediction[1:] - prediction[:19]) / 0.017, (target[1:] - target[:19]) / 0.017)
+        loss_xval = 2.000 * torch.nn.L1Loss()(pred[:,:,:24], target[:,:,:24])
+        loss_zval = 1.000 * torch.nn.L1Loss()(pred[:,:,24:], target[:,:,24:])
 
-        loss = loss_val + loss_vel
+        # loss_xvel = 0.050 * torch.nn.L1Loss()(pred[:,:,xVel], target[:,:,xVel])
+        loss_xvel = 0.050 * torch.nn.L1Loss()((pred[1:,:,:24] - pred[:19,:,:24]) / 0.017, (target[1:,:,:24] - target[:19,:,:24]) / 0.017)
+        loss_zvel = 0.015 * torch.nn.L1Loss()((pred[1:,:,24:] - pred[:19,:,24:]) / 0.017, (target[1:,:,24:] - target[:19,:,24:]) / 0.017)
+
+        loss = loss_xval + loss_zval + loss_xvel + loss_zvel
+        # loss = loss_xval + loss_zval
 
         optimizer.zero_grad()   # clear gradients for next train
         loss.backward()         # backpropagation, compute gradients
@@ -79,18 +107,21 @@ for t in range(epochs + 1):
 
         # log loss value
         epochLoss += loss.item()
+        xval += loss_xval.item()
+        zval += loss_zval.item()
+        xvel += loss_xvel.item()
+        zvel += loss_zvel.item()
 
     # just printing where training is
     if t % logFreq == 0:
         print("Epoch:", t, "\tLoss:", epochLoss, "\tRuntime:", (time.time() - epoch_time) * logFreq / 60, "minutes")
+        print(xval, zval, xvel, zvel)
     
     # log loss in tensorboard    
     writer.add_scalar('Stepper Loss', epochLoss, t)
 
 # sample input shape
-x   = torch.rand(1, 1, 56, device=device)
-h_t = torch.rand(1, 1, 512, device=device)
-c_t = torch.rand(1, 1, 512, device=device)
+x  = torch.rand(1, 1, 56, device=device)
 
 # export the model
 torch.onnx.export(